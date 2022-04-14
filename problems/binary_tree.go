58d5f380c32f4aeb570289e36a90d6f13f738dc3
snowcrystall <xuemin.cheng@gmail.com>
2022-04-14 12:01:57 +0800

    problems 111

--- problems/binary_tree.go
+++ problems/binary_tree.go
@@ -97,3 +97,25 @@ func height(root *TreeNode) int {
 	}
 	return int(math.Max(float64(leftHeight), float64(rightHeight))) + 1
 }
+
+//111. 二叉树的最小深度
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+func min(a, b int) int {
+
+	if a < b {
+		return a
+	}
+	return b
+}
