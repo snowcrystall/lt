8fe37f657b0cff4e7c244cac8132abea5ea99458
snowcrystall <xuemin.cheng@gmail.com>
2022-05-03 16:45:15 +0800

    problems 937

--- problems/simple/simple.go
+++ problems/simple/simple.go
@@ -1,4 +1,10 @@
-package problems
+package simple
+
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
 
 /* https://leetcode-cn.com/problems/number-of-1-bits/
  * 191. 位1的个数
@@ -163,4 +169,23 @@ func reverseBits(num uint32) uint32 {
 		num >>= 1
 	}
 	return rev
+}
+
+//937 https://leetcode-cn.com/problems/reorder-data-in-log-files/
+func reorderLogFiles(logs []string) []string {
+	sort.SliceStable(logs, func(i, j int) bool {
+		s, t := logs[i], logs[j]
+		s1 := strings.SplitN(s, " ", 2)[1]
+		s2 := strings.SplitN(t, " ", 2)[1]
+		isDigit1 := unicode.IsDigit(rune(s1[0]))
+		isDigit2 := unicode.IsDigit(rune(s2[0]))
+		if isDigit1 && isDigit2 {
+			return false
+		}
+		if !isDigit1 && !isDigit2 {
+			return s1 < s2 || s1 == s2 && s < t
+		}
+		return !isDigit1
+	})
+	return logs
 }
